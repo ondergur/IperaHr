9207410f72542cf772fd4a4a9d432509d463dc73
ondergur <ahmetondergur195@gmail.com>
2018-08-06 13:34:12 +0300

    companies.index blade, css-script stack in app.blade

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -1,24 +1,51 @@
 @extends('layouts.app')
 
+@push('css')
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+@endpush
+
 @section('content')
     <div class="container">
         <h1>Companies Index Page</h1>
 
         <table id="company_index_table" class="table">
-        <thead>
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Address</th>
-            <th scope="col">E-mail</th>
-            <th scope="col">Website</th>
-            <th scope="col">Actions</th>
-        </tr>
-        </thead>
-        <tbody>
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Address</th>
+                <th scope="col">Phone</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Website</th>
+            </tr>
+            </thead>
+            <tbody>
 
-        </tbody>
+            </tbody>
         </table>
 
     </div>
 @endsection
+
+@push('scripts')
+    <script type="text/javascript" charset="utf8"
+            src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+
+    <script>
+        $(function () {
+            $('#company_index_table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: '{{ route('company_get_data') }}',
+                columns: [
+                    {data: 'id', name: 'id'},
+                    {data: 'name', name: 'name'},
+                    {data: 'address', name: 'address'},
+                    {data: 'phone', name: 'phone'},
+                    {data: 'email', name: 'email'},
+                    {data: 'website', name: 'website'},
+                ]
+            });
+        });
+    </script>
+@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -19,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @stack('css')
 </head>
 <body>
     <div id="app">
@@ -75,5 +75,8 @@
             @yield('content')
         </main>
     </div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    @stack('scripts')
 </body>
 </html>
