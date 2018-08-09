a8a405b6c325335f61486ea72e4d367eaa57c902
ondergur <ahmetondergur195@gmail.com>
2018-08-09 14:29:15 +0300

    company index.blade

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -10,41 +10,42 @@
         {{Form::open(['route' => 'companies.index', 'method' => 'GET', 'id' => 'company_filter']) }}
         <div class="row justify-content-between">
             <div class="col-md">
-                {{Form::text('name', request('name') , ['class' => 'form-control', 'placeholder' => 'Filter Name'])}}
+                {{Form::text('name', request('name') , ['class' => 'form-control', 'placeholder' => 'Filter by Name'])}}
             </div>
             <div class="col-md">
-                {{Form::text('address', request('address') , ['class' => 'form-control', 'placeholder' => 'Filter Address'])}}
+                {{Form::text('address', request('address') , ['class' => 'form-control', 'placeholder' => 'Filter by Address'])}}
             </div>
             <div class="col-md">
-                {{Form::text('phone', request('phone') , ['class' => 'form-control', 'placeholder' => 'Filter Phone'])}}
+                {{Form::text('phone', request('phone') , ['class' => 'form-control', 'placeholder' => 'Filter by Phone'])}}
             </div>
             <div class="col-md">
-                {{Form::text('email', request('email') , ['class' => 'form-control', 'placeholder' => 'Filter E-mail'])}}
+                {{Form::text('email', request('email') , ['class' => 'form-control', 'placeholder' => 'Filter by E-mail'])}}
             </div>
             <div class="col-md">
-                {{Form::text('website', request('website') , ['class' => 'form-control', 'placeholder' => 'Filter Website'])}}
+                {{Form::text('website', request('website') , ['class' => 'form-control', 'placeholder' => 'Filter by Website'])}}
             </div>
             {{Form::submit('Filter', ['class' => 'btn btn-success my-2 my-sm-0'])}}
             {{Form::close()}}
         </div>
-        <table id="company_index_table" class="table">
-            <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">Address</th>
-                <th scope="col">Phone</th>
-                <th scope="col">E-mail</th>
-                <th scope="col">Website</th>
-                <th scope="col">Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-
-            </tbody>
-        </table>
 
     </div>
+    <table id="company_index_table" class="table">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Address</th>
+            <th scope="col">Phone</th>
+            <th scope="col">E-mail</th>
+            <th scope="col">Website</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+
 @endsection
 
 @push('scripts')
